afbba791c5d2bb4310d5452fc4d10a6617079c69
cerensolpan <cerensolpan@gmail.com>
2022-11-22 00:04:56 +0300

    Update App.js using router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./Home";
+import Units from "./Units";
+import Unit from "./Unit";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center container">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/units" element={<Units />}></Route>
+          <Route exact path="/units/:id" element={<Unit />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
