ba9e6d13094c9eca3d4c4b0d9b5ae36565524381
cerensolpan <cerensolpan@gmail.com>
2022-11-22 00:03:40 +0300

    Create Home, Units, Unit Detail Page

---
+++ src/Unit/index.js
@@ -0,0 +1,7 @@
+export default function Unit() {
+  return (
+    <div>
+      <h1 className="text-xl">Unit Detail Page</h1>
+    </div>
+  );
+}
---
+++ src/Units/index.js
@@ -0,0 +1,7 @@
+export default function Units() {
+  return (
+    <div>
+      <h1 className="text-xl">Units Page</h1>
+    </div>
+  );
+}
