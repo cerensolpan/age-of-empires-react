3caad1e3ecd5217b4f3177512a8424a6eec0d13d
cerensolpan <cerensolpan@gmail.com>
2022-11-22 23:29:32 +0300

    Create Loading Circle component and update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className="flex space-x-4 justify-end mb-8">
+    <div className="flex space-x-4 justify-end my-8">
       <div>
-        <Link to="/">Home</Link>
+        <Link className="border border-blue-200 bg-blue-200 px-4 py-2 rounded-lg hover:bg-blue-100 cursor-pointer" to="/">Home</Link>
       </div>
-      <div>
-        <Link to="/units">Units</Link>
+      <div >
+        <Link className="border border-blue-200 bg-blue-200 px-4 py-2 rounded-lg hover:bg-blue-100 cursor-pointer" to="/units">Units</Link>
       </div>
     </div>
   );
