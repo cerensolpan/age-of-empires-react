c3de7ccc004078f30d7d1c4aa55054cd02df331b
cerensolpan <cerensolpan@gmail.com>
2022-11-24 00:03:59 +0300

    Import global scss into index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./styles/global.scss"
 import App from "./App";
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
