861a072bea73462b6241aeacbddf2794e5b05164
cerensolpan <cerensolpan@gmail.com>
2022-11-24 00:03:30 +0300

    Add image to home page

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-xl">Home Page</h1>
+    <div className="space-y-12 flex flex-col justify-center items-center">
+      <h1 className="text-2xl text-blue-500">Home Page</h1>
+      <img src="/images/ageOfEmpires.jpeg" width={998} height={524} alt="ageOfEmpires" />
     </div>
   );
 }
