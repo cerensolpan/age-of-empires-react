cf399d6ae2629c1245ebfa1b4c2abdcedbf37f32
cerensolpan <cerensolpan@gmail.com>
2022-12-19 23:37:12 +0300

    Delete unnecessary files

--- src/Units/index.js
+++ src/Units/index.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import Table from "../components/Table";
 import { useDispatch, useSelector } from "react-redux";
-import { requestUnitListData } from "../actions/actionTypes";
 import LoadingCircle from "../components/LoadingCircle";
 import { fetchUnitList } from "./unitListSlice";
 import { unitListSelector } from "./unitListSlice";
