b096d156338e0c1909363f45151edaa4c7f4d16b
cerensolpan <cerensolpan@gmail.com>
2022-12-20 01:16:13 +0300

    Get render,screen and cleanup from render wrapper

--- src/Home/test/home.spec.js
+++ src/Home/test/home.spec.js
@@ -1,4 +1,4 @@
-import { render, screen, cleanup } from "@testing-library/react";
+import { render, screen, cleanup } from "../../common/helpers/renderWrapper";
 import Home from "../index";
 
 afterEach(cleanup);
