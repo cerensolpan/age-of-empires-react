ec72222dec9c0de328dfb0a68b97b357bd61737e
cerensolpan <cerensolpan@gmail.com>
2023-01-20 00:01:05 +0300

    Import json file to mongodb using node.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,39 @@
 const express = require("express");
 const router = require("./routes/index");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const fs = require("fs");
+const Units = require("./models/Units");
+
+dotenv.config({ path: "../.env" });
+
+const mongoURI = process.env.MONGODB_URL;
 
 const app = express();
-
 const PORT = process.env.PORT || 3500;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("db connected"))
+  .catch((e) => console.log("e", e));
+const data = JSON.parse(
+  fs.readFileSync("./age-of-empires-units.json", "utf-8")
+);
+
+// import data to MongoDB
+const importData = async () => {
+  try {
+    await Units.create(data.units);
+    console.log("data successfully imported");
+    // to exit the process
+    process.exit();
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+importData();
+
 app.use(router);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
